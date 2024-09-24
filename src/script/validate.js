884f53fadac8ca5da75121909e0377ce4d3614f5
Felipe de Melo <felipedemelodg@gmail.com>
2024-09-24 14:27:06 -0300

    alinhamento das assinaturas

--- src/script/validate.js
+++ src/script/validate.js
@@ -17,11 +17,10 @@ export function validate() {
 
 function gerarCertificado() {
     entradas.forEach((el, i, entrada) => {
-        console.log(valores[i].value,  entrada[i].innerHTML)
         const arr = valores[5].value.split('-')
         const arrPosicoes = [arr[0], arr[1], arr[2]] = [arr[2], arr[1], arr[0]]
         const arrComBarras = arrPosicoes.join().replace(/,/g, '/')
-
+        entrada[9].innerHTML = 'RG: ' + valores[9].value
         entrada[1].innerHTML = 'CPF: ' + valores[1].value
         entrada[5].innerHTML = arrComBarras
         entrada[i].innerHTML = valores[i].value
