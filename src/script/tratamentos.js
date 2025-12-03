156ef6f096154dbcbbd86f0d52958cac46b29ae8
felipedemelodg <felipedemelodg@gmail.com>
2025-12-03 13:43:46 -0300

    adição de lista dos instrutores

--- src/script/tratamentos.js
+++ src/script/tratamentos.js
@@ -3,24 +3,15 @@ import { fabricaDeObjetos, tratamentoBasico, tratamentoEspecifico, tratar } from
 
 
 const objNomeDoAluno = fabricaDeObjetos(nomeAluno, 'nomeAluno', erroNomeAluno, 3, false, true)
-
 const objCPFDoAluno = fabricaDeObjetos(cpfAluno, 'cpfAluno', erroCpfAluno, 11, true, false)
-
 const objTreinamento = fabricaDeObjetos(nomeTreinamento, 'nomeTreinamento', erroNomeTreinamento, 5, false, true)
-
 const objTextoComplementar = fabricaDeObjetos(textoComplementar, 'textoComplementar', errotextoComplementar, 5, false, false)
-
 const objTratarDatar = fabricaDeObjetos(data, 'data', erroData, 5, false, false)
 const objPromovidoPor = fabricaDeObjetos(promovidoPor, 'promovidoPor', erroPromovidoPor, 5, false, false)
-
 const objCargaHoraria = fabricaDeObjetos(cargaHoraria, 'cargaHoraria', erroCargaHoraria, 0, false, false)
-
 const objNRs = fabricaDeObjetos(nr, 'nr', erroNr, 3, false, false)
-
 const objNomeInstrutor = fabricaDeObjetos(nomeInstrutor, 'nomeInstrutor', erroNomeInstrutor, 3, false, true)
-
 const objRgInstrutor = fabricaDeObjetos(rgDoInstrutor, 'rgDoInstrutor', erroRgDoInstrutor, 12, true, false)
-
 const objCertificacaoDoInstrutor = fabricaDeObjetos(certificacaoDoInstrutor, 'certificacaoDoInstrutor', erroCertificacaoDoInstrutor, 5, false, false)
 
 
